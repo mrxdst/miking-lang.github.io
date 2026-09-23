dec7ca647720599342c1be80fe5312771b386051
David Turesson <davidtur@kth.se>
2026-09-23 15:24:29 +0200

    monaco editor

--- src/pages/playground.tsx
+++ src/pages/playground.tsx
@@ -6,6 +6,8 @@ import type { FromWorkerMessage, ToWorkerMessage } from "./playground.worker";
 import "@xterm/xterm/css/xterm.css";
 import { Terminal } from "@xterm/xterm";
 import { FitAddon } from "@xterm/addon-fit";
+import Editor from '@monaco-editor/react';
+import type * as monaco from "monaco-editor";
 
 const STORAGE_KEY = "miking-playground-input";
 const DEFAULT_INPUT = [
@@ -20,45 +22,48 @@ function initWorker(): Worker {
 
 export default function Playground(): JSX.Element {
     const [worker, setWorker] = useState<Worker>(initWorker);
-    const outputRef = useRef<HTMLDivElement>(null);
-    const termRef = useRef<Terminal>(new Terminal({convertEol: true, disableStdin: true}));
-    const termFitRef = useRef(new FitAddon());
+    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+    const termContainerRef = useRef<HTMLDivElement>(null);
+    const termRef = useRef<Terminal | null>(null);
 
-    const [input, setInput] = useState(() => sessionStorage.getItem(STORAGE_KEY) || localStorage.getItem(STORAGE_KEY) || DEFAULT_INPUT);
+    const [defaultInput] = useState(() => sessionStorage.getItem(STORAGE_KEY) || localStorage.getItem(STORAGE_KEY) || DEFAULT_INPUT);
 
     const [running, setRunning] = useState(false);
 
     // Terminal
     useEffect(() => {
-        if (!outputRef.current) return;
+        if (!termContainerRef.current) return;
 
-        termRef.current.open(outputRef.current);
-        termRef.current.loadAddon(termFitRef.current);
-        termFitRef.current.fit();
+        const term = termRef.current = new Terminal({convertEol: true, disableStdin: true});
+        const termFit = new FitAddon();
+
+        term.open(termContainerRef.current);
+        term.loadAddon(termFit);
+        termFit.fit();
 
         const resizeObserver = new ResizeObserver(() => {
-            termFitRef.current.fit();
+            termFit.fit();
         });
 
-        resizeObserver.observe(outputRef.current);
+        resizeObserver.observe(termContainerRef.current);
 
         return () => {
             resizeObserver.disconnect();
-            termRef.current.dispose();
+            termFit.dispose();
         };
     }, []);
 
     // Worker
     useEffect(() => {
         worker.addEventListener("message", handleMessage);
-        
+
         return () => { worker.terminate(); }
 
         function handleMessage(event: MessageEvent<FromWorkerMessage>) {
             const msg = event.data;
             switch (msg.type) {
                 case "print": {
-                    termRef.current.write(msg.text);
+                    termRef.current?.write(msg.text);
                     break;
                 }
                 case "exit": {
@@ -69,17 +74,21 @@ export default function Playground(): JSX.Element {
         }
     }, [worker])
 
+    const handleEditorDidMount = useCallback((editor: monaco.editor.IStandaloneCodeEditor) => {
+        editorRef.current = editor;
+    }, []);
+
     // Save input
-    useEffect(() => {
-        sessionStorage.setItem(STORAGE_KEY, input);
-        localStorage.setItem(STORAGE_KEY, input);
-    }, [input]);
+    const handleEditorChange = useCallback((value: string | undefined) => {
+        sessionStorage.setItem(STORAGE_KEY, value || "");
+        localStorage.setItem(STORAGE_KEY, value || "");
+    }, []);
 
     const handleRun = useCallback(() => {
         setRunning(true);
-        const msg: ToWorkerMessage = { input };
+        const msg: ToWorkerMessage = { input: editorRef.current?.getValue() || '' };
         worker.postMessage(msg);
-    }, [input, worker]);
+    }, [worker]);
 
     const handleAbort = useCallback(() => {
         setRunning(false);
@@ -92,14 +101,20 @@ export default function Playground(): JSX.Element {
             <div className="container margin-vert--lg">
                 <div className="row">
                     <div className={clsx("col col--6", styles.inputCol)}>
-                        <textarea className={styles.input} wrap="off" value={input} onChange={(e) => setInput(e.target.value)} />
+                        <div className={styles.input}>
+                            <Editor
+                                defaultValue={defaultInput}
+                                onMount={handleEditorDidMount}
+                                onChange={handleEditorChange}
+                            />
+                        </div>
                         {running
                             ? <button className={styles.btn} onClick={handleAbort}>Abort</button>
                             : <button className={styles.btn} onClick={handleRun}>Run</button>
                         }
                     </div>
                     <div className="col col--6">
-                        <div className={styles.output} ref={outputRef}/>
+                        <div className={styles.output} ref={termContainerRef}/>
                     </div>
                 </div>
             </div>
